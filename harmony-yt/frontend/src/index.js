b632e88b9fb85f53e7830401c390f4cbeb4ff51e
mohammadaamirsohail <amir2002sohail@gmail.com>
2026-06-27 00:42:32 +0530

    add PWA support and Harmony by Aamir name

--- harmony-yt/frontend/src/index.js
+++ harmony-yt/frontend/src/index.js
@@ -5,3 +5,10 @@ import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<React.StrictMode><App /></React.StrictMode>);
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/sw.js")
+      .then(() => console.log("Service Worker registered"))
+      .catch((err) => console.log("SW registration failed:", err));
+  });
+}

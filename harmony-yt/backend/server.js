46254d6c1de91500cf6e18f04d36443ee2e3ea23
mohammadaamirsohail <amir2002sohail@gmail.com>
2026-06-26 23:53:43 +0530

    fix CORS for vercel

--- harmony-yt/backend/server.js
+++ harmony-yt/backend/server.js
@@ -6,7 +6,9 @@ const axios   = require("axios");
 const app  = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: ["http://localhost:3000", "https://my-music-application.vercel.app"]
+}));
 app.use(express.json());
 
 // ─── API Key Rotation ─────────────────────────────────────────────────────────
